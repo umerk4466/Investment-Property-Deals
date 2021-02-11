6ef735a7d16b5c4c6edf28565b4495c4d7739402
umerk4466 <umerk4466@gmail.com>
2021-02-11 16:30:49 +0000

    Updated

--- PropertyDeals/users/urls.py
+++ PropertyDeals/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', auth_views.LoginView.as_view(template_name='Users/home.html',redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
 
 
 ]
