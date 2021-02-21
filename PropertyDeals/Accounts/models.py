8e6ebf8e063d403509a963d26855c49462831d8b
umerk4466 <umerk4466@gmail.com>
2021-02-21 00:42:32 +0000

    Updated

--- PropertyDeals/Accounts/models.py
+++ PropertyDeals/Accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class User(AbstractUser):
+  USER_TYPE_CHOICES = (
+      (1, 'investor'),
+      (2, 'deal_sourcer'),
+  )
+
+  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES,  blank=True, null=True)
