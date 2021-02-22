f8fc534a8f8a656ac9df5f847b678987990ca140
umerk4466 <umerk4466@gmail.com>
2021-02-22 01:31:47 +0000

    Updated

--- PropertyDeals/Accounts/admin.py
+++ PropertyDeals/Accounts/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+# import models
+from .models import Profile
 from .models import User
-# Register your models here.
-admin.site.register(User)
+
+
+
+# display profile inside of user model in admin panel
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+    # add field which created in custom User model
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            None,  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'user_type',
+                ),
+            },
+        ),
+    )
+    
+
+# Register all the models in admin panel
+admin.site.register(User, CustomUserAdmin)
--- PropertyDeals/Accounts/models.py
+++ PropertyDeals/Accounts/models.py
@@ -5,19 +5,40 @@ from django.db import models
 
 class User(AbstractUser):
   USER_TYPE_CHOICES = (
-      (1, 'investor'),
-      (2, 'deal_sourcer'),
-      (3, 'admin'),
+      (1, 'Investor'),
+      (2, 'Deal sourcer'),
+      (3, 'Admin'),
+      (4, 'Super admin'),
+
   )
 
-  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES,  blank=True, null=True)
+  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES,  default=USER_TYPE_CHOICES[0][0])
 # https://github.com/pydanny/multiple-user-types-django/blob/master/spybook/users/models.py
 
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = models.TextField(blank=True)
+  picture = models.ImageField(
+        upload_to='profile_images', blank=True, null=True)
+  phone_number = models.CharField(max_length=30, blank=True)
+  display_name = models.CharField(max_length=255, blank=True)
+  date_of_birth = models.DateField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
+  country = models.CharField(max_length=255, blank=True)
+  city = models.CharField(max_length=255, blank=True)
+  GENDER_CHOICES = (
+        ('F', 'Female',),
+        ('M', 'Male',),
+    )
+  gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+
+  def __str__(self):
+        return self.user.username
 
 class Investor(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class Deal_Sourcer(models.Model):
+class DealSourcer(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Admin(models.Model):
