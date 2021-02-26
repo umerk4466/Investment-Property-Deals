fedcbb494bd9b6789b71bf4353e18c4719adfe3d
umerk4466 <umerk4466@gmail.com>
2021-02-26 17:39:23 +0000

    Updated

--- PropertyDeals/Deals/forms.py
+++ PropertyDeals/Deals/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from Deals.price_lists import PRICE_LIST
+from Deals.choise_field_lists import PRICE_LIST,BEDROOM_LIST
 
 class SearchDeal(forms.Form):
     place = forms.CharField(label="Search using 'town name', 'postcode'",max_length=255, 
                     widget=forms.TextInput(attrs={'placeholder': 'e.g. ‘York’, ‘LU3’ or ‘LU21HT’'}))
-    min_price_choises = forms.ChoiceField(choices = PRICE_LIST, initial='01', required = True,)
+    min_price_choises = forms.ChoiceField(label="Min Price (£)", choices = PRICE_LIST, initial='0', required = False)
+    max_price_choises = forms.ChoiceField(label="Max Price (£)", choices = PRICE_LIST, initial='0', required = False,)
+    min_bedroom = forms.ChoiceField(label="Min Bedroom", choices = BEDROOM_LIST, initial='0', required = False,)
+    max_bedroom = forms.ChoiceField(label="Max Bedroom", choices = BEDROOM_LIST, initial='0', required = False,)
+
+
