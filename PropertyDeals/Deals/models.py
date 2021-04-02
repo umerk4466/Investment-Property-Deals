f4cd2feb87210bcd85d8bf39f33b1fecafde00e7
umerk4466 <umerk4466@gmail.com>
2021-04-03 00:01:24 +0100

    Updated

--- PropertyDeals/Deals/models.py
+++ PropertyDeals/Deals/models.py
@@ -13,7 +13,8 @@ class Property(models.Model):
     property_type = models.ForeignKey(PropertyType, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    
+
+
     def __str__(self):
         return self.title
 
