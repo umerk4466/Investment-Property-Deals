648a10735ac13ddb77062934721352e63e8cb56f
umerk4466 <umerk4466@gmail.com>
2021-04-07 20:46:04 +0100

    Updated

--- PropertyDeals/Deals/static/Deals/js/properties_listing.js
+++ PropertyDeals/Deals/static/Deals/js/properties_listing.js
@@ -4,3 +4,12 @@ $( document ).ready(function() {
     $('#id_min_bedroom option:first').text('Min Beds');
     $('#id_max_bedroom option:first').text('Max Beds');
 });
+
+
+
+$('#filter-btn').click(function() {
+    $('#custom-modal').slideToggle("fast", function() {
+        // Animation complete.
+        $('.filter-btn').css("background-color", "white");
+      });
+});
