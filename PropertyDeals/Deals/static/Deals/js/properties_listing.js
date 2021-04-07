0e9e57aa0e48f47cbec33b303e5a5bd29ba66cc5
umer4466 <umer4466@gmail.com>
2021-04-07 18:13:56 +0100

    Updated

---
+++ PropertyDeals/Deals/static/Deals/js/properties_listing.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $('#id_min_price option:first').text('Min Price');
+    $('#id_max_price option:first').text('Max Price');
+    $('#id_min_bedroom option:first').text('Min Beds');
+    $('#id_max_bedroom option:first').text('Max Beds');
+});
