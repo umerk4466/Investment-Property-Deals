6cb93f8d1bd01d936cb6382c1b06b10448a868a4
umerk4466 <umerk4466@gmail.com>
2021-04-08 16:45:35 +0100

    Updated

--- PropertyDeals/Deals/static/Deals/js/properties_listing.js
+++ PropertyDeals/Deals/static/Deals/js/properties_listing.js
@@ -5,11 +5,18 @@ $( document ).ready(function() {
     $('#id_max_bedroom option:first').text('Max Beds');
 });
 
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if($(e.target).hasClass("hide-filter"))
+        alert("s");
+    //    e.addClass('display-filter');
+    else
+       e.style.display = 'block';
+ }
 
-
-$('#filter-btn').click(function() {
-    $('#custom-modal').slideToggle("fast", function() {
-        // Animation complete.
-        $('.filter-btn').css("background-color", "white");
-      });
-});
+// $('#filter-btn').click(function() {
+//     $('#custom-modal').slideToggle("fast", function() {
+//         // Animation complete.
+//         // $('.filter-btn').css("background-color", "white");
+//       });
+// });
